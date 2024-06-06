1fc42c56b7cd017b19ea6f6738075269f7fe4342
dsw52673 <dsw52673@student.dsw.edu.pl>
2024-06-06 13:44:14 +0000

    dodano do flagi --logs mozliwosc wpisania ilosci plikow

--- skrypt.sh
+++ skrypt.sh
@@ -3,7 +3,13 @@ if [[ "$1" == "--date" ]]; then
 fi
 
 if [[ "$1" == "--logs" ]]; then
-  for i in {1..10}; do
-    echo "Plik: log$i.txt, Skrypt: $0, Data: $(date)" > "log$i.txt"
-  done
+  if [[ -n "$2" ]]; then
+    for i in $(seq 1 $2); do
+      echo "Plik: log$i.txt, Skrypt: $0, Data: $(date)" > "log$i.txt"
+    done
+  else
+    for i in {1..100}; do
+      echo "Plik: log$i.txt, Skrypt: $0, Data: $(date)" > "log$i.txt"
+    done
+  fi
 fi
