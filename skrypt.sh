2739e08efeea17ad31995a70a7b8627142a3b7f7
dsw52673 <dsw52673@student.dsw.edu.pl>
2024-06-06 13:41:31 +0000

    dodano flage --logs do pliku skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,9 @@
 if [[ "$1" == "--date" ]]; then
   echo "Dzisiejsza data: $(date)"
 fi
+
+if [[ "$1" == "--logs" ]]; then
+  for i in {1..10}; do
+    echo "Plik: log$i.txt, Skrypt: $0, Data: $(date)" > "log$i.txt"
+  done
+fi
