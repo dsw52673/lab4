b47d872c651a0015e6c7fa8b0dd316214225f53c
dsw52673 <dsw52673@student.dsw.edu.pl>
2024-06-06 13:55:38 +0000

    dodano nowe flagi

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,8 @@
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
   echo "Dzisiejsza data: $(date)"
 fi
 
-if [[ "$1" == "--logs" ]]; then
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
   if [[ -n "$2" ]]; then
     for i in $(seq 1 $2); do
       echo "Plik: log$i.txt, Skrypt: $0, Data: $(date)" > "log$i.txt"
@@ -14,9 +14,11 @@ if [[ "$1" == "--logs" ]]; then
   fi
 fi
 
-if [[ "$1" == "--help" ]]; then
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
   echo "Dostępne opcje:"
-  echo "--date: Wyświetla dzisiejszą datę"
-  echo "--logs [liczba]: Generuje określoną liczbę plików logów, domyślnie 100"
-  echo "--help: Wyświetla wszystkie dostępne opcje"
+  echo "--date, -d: Wyświetla dzisiejszą datę"
+  echo "--logs, -l [liczba]: Generuje określoną liczbę plików logów, domyślnie 100"
+  echo "--help, -h: Wyświetla wszystkie dostępne opcje"
 fi
+
+
